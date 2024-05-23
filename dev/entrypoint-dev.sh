330230a07e3b01cdfc826384bd0520d839924e1e
Julien Loizelet <julienloizelet@okaeli.com>
2024-05-23 18:24:09 +0900

    test(dev): Install prod requirements for dev docker

--- dev/entrypoint-dev.sh
+++ dev/entrypoint-dev.sh
@@ -3,7 +3,7 @@
 # Correct working directory
 cd /opt/opencti-crowdsec || exit
 
-pip3 install --no-cache-dir -r requirements-dev.txt
+pip3 install --no-cache-dir -r requirements.txt
 
 # Idle indefinitely
 while true; do
